a8ec56e17f9cedbda5a15a64ea2321344d4f4c25
Miggie Garcia <60056056+miggiegarcia@users.noreply.github.com>
2020-03-07 23:57:03 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+let start = document.querySelector('button')
+let key ="8fcb1c44-2044-4c3e-9ca0-9fb503a9658c"
+
+start.addEventListener('click', () =>{
+  // console.log("mig")
+  fetch(`https://api.kanye.rest`)
+    .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
+    .then(response => {
+       console.log(response.quote)
+      document.getElementById('kanye').innerHTML=response.quote
+    })
+    fetch( `https://content.guardianapis.com/search?api-key=8fcb1c44-2044-4c3e-9ca0-9fb503a9658c`)
+      .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
+      .then(response => {
+   document.getElementById('guard').innerHTML= response.response.results[0].webTitle
+
+ // console.log(response.response.results[0].webTitle)
+
+
+})
+})
